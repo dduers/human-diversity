e6ae330fedb2d3bf11ac9f3fb5d6022ac322a492
Daniel Duersteler <daniel.duersteler@gmail.com>
2021-09-25 07:43:58 +0200

    updated meta tages

--- public/index.php
+++ public/index.php
@@ -3,7 +3,10 @@
     <head>
         <title>Human Diversity</title>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Look for humans with different skills, attitudes and properties :)" />
+        <meta name="keywords" content="human, diversity, skills, skill, soft, softskill, softskills, humour">
+        <meta name="author" content="Daniel Duersteler">
         <link rel="stylesheet" type="text/css" href="css/style.css" />
     </head>
     <body>
