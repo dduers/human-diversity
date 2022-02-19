8d193cc766b91495715d8c92d4e6d01b36ededd2
Daniel Duersteler <daniel.duersteler@gmail.com>
2022-02-19 22:44:30 +0100

    working on templates and controller boilerplates

---
+++ src/Controller/recovery.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dduers\HumanDiversity\Controller;
+
+use Dduers\HumanDiversity\AppController;
+
+final class recovery extends AppController
+{
+    function get()
+    {
+    }
+
+    function post()
+    {
+    }
+}
---
+++ src/Controller/register.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dduers\HumanDiversity\Controller;
+
+use Dduers\HumanDiversity\AppController;
+
+final class register extends AppController
+{
+    function get()
+    {
+    }
+
+    function post()
+    {
+    }
+}
