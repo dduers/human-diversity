5a6975cae38ceb5c2d24d5b135a50c78f371ed1b
Daniel Duersteler <daniel.duersteler@gmail.com>
2022-02-19 21:25:28 +0100

    working on templates

--- src/Controller/login.php
+++ src/Controller/login.php
@@ -6,7 +6,7 @@ namespace Dduers\HumanDiversity\Controller;
 
 use Dduers\HumanDiversity\AppController;
 
-final class home extends AppController
+final class login extends AppController
 {
     function get()
     {
---
+++ src/Controller/search.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dduers\HumanDiversity\Controller;
+
+use Dduers\HumanDiversity\AppController;
+
+final class search extends AppController
+{
+    function get()
+    {
+    }
+
+    function post()
+    {
+    }
+}
